093cd71ee3c9e3a6e084c26833822e745bbf00c9
akhshyganesh <akhshyganeshb@gmail.com>
2026-06-13 12:10:19 +0530

    implement login form and associated JavaScript functionality

--- login/login.js
+++ login/login.js
@@ -1,1 +1,12 @@
 console.log("Login JS file is loaded");
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    console.log("Username:", username);
+    console.log("Password:", password);
+    // Here you can add your login logic, such as sending a request to the server
+});
